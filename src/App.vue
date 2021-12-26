a1bb80659d9ea64027bfa6cca6130664f2cba96c
Katser <gray84@tut.by>
2021-12-26 16:59:59 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -3,44 +3,24 @@
     <!-- <Navbar v-if="$store.getters.getUser.isAuth" /> -->
     <v-row v-if="$store.getters.loaded != 'completed'" class="fill-height" align-content="center" justify="center">
       <v-col cols="12" class="text-center">
-        <v-icon x-large :color="$store.getters.getFromLoadingStatus.color">{{
-          $store.getters.getFromLoadingStatus.mdi
-        }}</v-icon>
+        <v-icon x-large :color="$store.getters.getFromLoadingStatus.color">{{ $store.getters.getFromLoadingStatus.mdi }}</v-icon>
       </v-col>
       <v-col class="title text-center" cols="12">
         {{ $store.getters.getFromLoadingStatus.status }}
       </v-col>
       <v-col cols="6">
-        <v-progress-linear
-          :color="$store.getters.getFromLoadingStatus.color"
-          indeterminate
-          rounded
-          height="6"
-        ></v-progress-linear>
+        <v-progress-linear :color="$store.getters.getFromLoadingStatus.color" indeterminate rounded height="6"></v-progress-linear>
       </v-col>
     </v-row>
     <v-main class="secondary">
       <router-view></router-view>
     </v-main>
-    <v-snackbar
-      multi-line
-      app
-      shaped
-      transition="fab-transition"
-      outlined
-      elevation="20"
-      class="primary"
-      :class="'text-'+$store.getters.getAlert.type"
-      v-model="$store.getters.getAlert.show"
-      timeout="2000"
-    >
+    <!-- <v-snackbar multi-line app shaped transition="fab-transition" outlined elevation="20" class="primary" :class="'text-' + $store.getters.getAlert.type" v-model="$store.getters.getAlert.show" timeout="2000">
       <template>
-        <v-icon class="mb-1" :color="$store.getters.getAlert.type"
-          >mdi {{ $store.getters.getAlert.mdi }}</v-icon
-        >
+        <v-icon class="mb-1" :color="$store.getters.getAlert.type">mdi {{ $store.getters.getAlert.mdi }}</v-icon>
         <span class="title">{{ " " + $store.getters.getAlert.message }}</span>
       </template>
-    </v-snackbar>
+    </v-snackbar> -->
   </v-app>
 </template>
 <script>
@@ -48,20 +28,15 @@ export default {
   async mounted() {
     console.log("loading", this.$store.getters.loaded);
     await this.$store.dispatch("getUserInfo");
-    if (
-      !this.$store.getters.getUser.isAuth &&
-      location.pathname.indexOf("login") == -1
-    ) {
+    if (!this.$store.getters.getUser.isAuth && location.pathname.indexOf("login") == -1) {
       this.$router.push("/login");
     }
-    this.$vuetify.theme.themes.light.primary = this.$store.getters.getSettings.colors.light.primary
-    this.$vuetify.theme.themes.light.secondary = this.$store.getters.getSettings.colors.light.secondary
-    this.$vuetify.theme.themes.light.accent = this.$store.getters.getSettings.colors.light.accent
+    this.$vuetify.theme.themes.light.primary = this.$store.getters.getSettings.colors.light.primary;
+    this.$vuetify.theme.themes.light.secondary = this.$store.getters.getSettings.colors.light.secondary;
+    this.$vuetify.theme.themes.light.accent = this.$store.getters.getSettings.colors.light.accent;
   },
   components: {},
-  created() {
-    
-  },
+  created() {},
 };
 </script>
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,84 +1,47 @@
-import '@mdi/font/css/materialdesignicons.css'
-import Vue from 'vue'
-import App from './App.vue'
-import Vuetify from 'vuetify/lib'
-import vuetify from './plugins/vuetify';
-import store from './store'
-import router from './router'
-import mixins from './mixins'
-import './directives'
-import firebase from "firebase/app"
-import 'firebase/auth'
-import 'firebase/database'
-import './registerServiceWorker'
+import "@mdi/font/css/materialdesignicons.css";
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify/lib";
+import vuetify from "./plugins/vuetify";
+import store from "./store";
+import router from "./router";
+import mixins from "./mixins";
+import "./directives";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "./registerServiceWorker";
 import VChart from "vue-echarts";
-import {
-  use
-} from 'echarts/core'
-
+import { use } from "echarts/core";
 
 // import ECharts modules manually to reduce bundle size
-import {
-  CanvasRenderer
-} from 'echarts/renderers'
-import {
-  LineChart,
-  BarChart,
-  LinesChart,
-} from 'echarts/charts'
-import {
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent,
-  GridComponent,
-  DatasetComponent,
-  DataZoomComponent,
-  DataZoomInsideComponent,
-  ToolboxComponent,
-  DataZoomSliderComponent,
-  MarkLineComponent,
-} from 'echarts/components'
+import { CanvasRenderer } from "echarts/renderers";
+import { LineChart, BarChart, LinesChart } from "echarts/charts";
+import { TitleComponent, TooltipComponent, LegendComponent, GridComponent, DatasetComponent, DataZoomComponent, DataZoomInsideComponent, ToolboxComponent, DataZoomSliderComponent, MarkLineComponent } from "echarts/components";
+use([CanvasRenderer, LineChart, BarChart, LinesChart, TitleComponent, TooltipComponent, LegendComponent, GridComponent, DatasetComponent, DataZoomComponent, DataZoomInsideComponent, ToolboxComponent, DataZoomSliderComponent, MarkLineComponent]);
+Vue.component("v-chart", VChart);
+Vue.use(Vuetify);
 
-use([
-  CanvasRenderer,
-  LineChart,
-  BarChart,
-  LinesChart,
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent,
-  GridComponent,
-  DatasetComponent,
-  DataZoomComponent,
-  DataZoomInsideComponent,
-  ToolboxComponent,
-  DataZoomSliderComponent,
-  MarkLineComponent,
-]);
-Vue.component('v-chart', VChart)
-Vue.use(Vuetify)
-
-new Vuetify({})
-Vue.mixin(mixins)
-Vue.config.productionTip = false
+new Vuetify({});
+Vue.mixin(mixins);
+Vue.config.productionTip = false;
 firebase.initializeApp({
-  apiKey: "AIzaSyDRyqzAgmQcV9sJLQSBv6uhAxkVG2T34ns",
+  apiKey: process.env.VUE_APP_FIREBASE_API,
   authDomain: "vf-salary.firebaseapp.com",
   projectId: "vf-salary",
   storageBucket: "vf-salary.appspot.com",
-  messagingSenderId: "87602971040",
-  appId: "1:87602971040:web:930eca2b249e926f22a4c5",
-  measurementId: "G-P9EZH9HWGP"
-})
-let app
+  messagingSenderId: process.env.VUE_APP_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
+});
+let app;
 firebase.auth().onAuthStateChanged(() => {
   if (!app) {
     app = new Vue({
       vuetify,
       store,
       router,
-      render: h => h(App)
-    }).$mount('#app')
+      render: (h) => h(App),
+    }).$mount("#app");
   }
-
-})
+});
